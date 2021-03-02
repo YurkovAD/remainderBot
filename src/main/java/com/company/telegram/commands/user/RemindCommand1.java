2f950cf3d7332ac05a3f2b9338195180b751eef6
yurkov.ad <yurkov.artem@skpari.ru>
2021-03-02 17:02:09 +0300

    added file

---
+++ src/main/java/com/company/telegram/commands/user/RemindCommand1.java
@@ -0,0 +1,33 @@
+//package com.company.telegram.commands.user;
+//
+//import com.company.utils.Utils;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
+//import org.telegram.telegrambots.meta.api.objects.Chat;
+//import org.telegram.telegrambots.meta.api.objects.User;
+//import org.telegram.telegrambots.meta.bots.AbsSender;
+//
+///**
+// * Created by yurkov.ad on 17.02.2021.
+// */
+//public class RemindCommand extends BotCommand {
+//
+//    public RemindCommand(String commandIdentifier, String description) {
+//        super(commandIdentifier, description);
+//    }
+//
+//    @Override
+//    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+//        String userName = Utils.getUserName(user);
+//
+//        logger.debug(String.format("Пользователь %s. Начато выполнение команды %s", userName,
+//                this.getCommandIdentifier()));
+//
+//
+//        logger.debug(String.format("Пользователь %s. Завершено выполнение команды %s", userName,
+//                this.getCommandIdentifier()));
+//    }
+//
+//    private Logger logger = LoggerFactory.getLogger(RemindCommand.class);
+//}
