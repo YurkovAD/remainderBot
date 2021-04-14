0b7bb5127e5017c920d8da8b8f777c57bcfb20a6
yurkov.ad <yurkov.artem@skpari.ru>
2021-04-14 18:28:49 +0300

    upgrade RemoveCommand

--- src/main/java/com/company/telegram/commands/user/RemoveCommand.java
+++ src/main/java/com/company/telegram/commands/user/RemoveCommand.java
@@ -7,13 +7,9 @@ import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.bots.AbsSender;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
 
 import static com.company.telegram.commands.user.RemindCommand.taskList;
 
@@ -33,71 +29,68 @@ public class RemoveCommand extends BotCommand implements BotMessageSender {
 
         if(taskListValidator.validate(taskList, task, absSender, user, message, this.getCommandIdentifier())) {
             //find
-            BotTask botTask = null;
-            List<BotTask> tmpTasks = new ArrayList<>();
-            taskList.forEach(bt -> {
-//                try {
-                    if(bt.getBotMessage().getMessge().equals(task)) {
-                        tmpTasks.add(bt);
-                    }
-//                } catch(Exception e) {
-//
-//                }
-            });
+//            BotTask botTask;
+//            List<BotTask> tmpTasks = new ArrayList<>();
+//            taskList.forEach(bt -> {
+////                try {
+//                    if(bt.getBotMessage().getMessge().equals(task)) {
+//                        tmpTasks.add(bt);
+//                    }
+////                } catch(Exception e) {
+////
+////                }
+//            });
+
             //delete
-            if(tmpTasks.size() == 0 || tmpTasks.isEmpty()){
+            if(taskList.size() == 0 || taskList.isEmpty()){
                 message.setText("В моём списке нет такой задачи!");
                 sendMess(absSender, message);
-            } else if(tmpTasks.size() == 1 ){
-                botTask = tmpTasks.get(0);
-                botTask.deleteTask(botTask);
-                message.setText("Задание " + botTask.getBotMessage().getMessge() + " удалено!");
-                sendMess(absSender, message);
             } else {
-//                SimpleDateFormat formater = new SimpleDateFormat("HH:mm");
-//                message.setText("Введиет порядковый номер задания, которое нужно удалить:\r\n");
-//                tmpTasks.forEach(tmpT -> {
-//                    message.setText(message.getText() + tmpT.getBotMessage().getMessge() + ", " + formater.format(tmpT.getBotMessage().getDateTime()) + "\r\n");
-//                });
-//                sendMess(absSender, message);
-                sendMess(absSender, createDeleteKeyboard(message, tmpTasks));
-
+                SimpleDateFormat formater = new SimpleDateFormat("HH:mm");
+                taskList.forEach(bt -> {
+                    String s = bt.getBotMessage().getMessge() + ", " + formater.format(bt.getBotMessage().getDateTime());
+                    if(s.equals(task)) {
+                        BotTask botTask = bt;
+                        botTask.deleteTask(botTask);
+                        message.setText("Задание " + botTask.getBotMessage().getMessge() + " удалено!");
+                        sendMess(absSender, message);
+                    }
+                });
             }
         }
     }
-
-    private SendMessage createDeleteKeyboard(SendMessage message, List<BotTask> tasks/*, AbsSender absSender*/){
-        SimpleDateFormat formater = new SimpleDateFormat("HH:mm");
-        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
-        List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<>();
-        tasks.forEach( task -> {
-            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
-            inlineKeyboardButton.setText(task.getBotMessage().getMessge() + ", " + formater.format(task.getBotMessage().getDateTime()));
-            inlineKeyboardButton.setCallbackData(callbackData(task/*, message, absSender*/));
-            keyboardButtonsRow.add(inlineKeyboardButton);
-            rowList.add(keyboardButtonsRow);
-        });
-//        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
-//        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
-//        inlineKeyboardButton1.setText("Тык");
-//        inlineKeyboardButton1.setCallbackData("Button \"Тык\" has been pressed");
-//        inlineKeyboardButton2.setText("Тык2");
-//        inlineKeyboardButton2.setCallbackData("Button \"Тык2\" has been pressed");
+//    private SendMessage createDeleteKeyboard(SendMessage message, List<BotTask> tasks/*, AbsSender absSender*/){
+//        SimpleDateFormat formater = new SimpleDateFormat("HH:mm");
+//        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+//        List<List<InlineKeyboardButton>> rowList = new ArrayList<>();
+//        List<InlineKeyboardButton> keyboardButtonsRow = new ArrayList<>();
+//        tasks.forEach( task -> {
+//            InlineKeyboardButton inlineKeyboardButton = new InlineKeyboardButton();
+//            inlineKeyboardButton.setText(task.getBotMessage().getMessge() + ", " + formater.format(task.getBotMessage().getDateTime()));
+//            inlineKeyboardButton.setCallbackData(callbackData(task/*, message, absSender*/));
+//            keyboardButtonsRow.add(inlineKeyboardButton);
+//        });
+//        rowList.add(keyboardButtonsRow);
+////        InlineKeyboardButton inlineKeyboardButton1 = new InlineKeyboardButton();
+////        InlineKeyboardButton inlineKeyboardButton2 = new InlineKeyboardButton();
+////        inlineKeyboardButton1.setText("Тык");
+////        inlineKeyboardButton1.setCallbackData("Button \"Тык\" has been pressed");
+////        inlineKeyboardButton2.setText("Тык2");
+////        inlineKeyboardButton2.setCallbackData("Button \"Тык2\" has been pressed");
+////
+////        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
+//////        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Fi4a").setCallbackData("CallFi4a"));
+////        keyboardButtonsRow2.add(inlineKeyboardButton2);
+////        rowList.add(keyboardButtonsRow2);
+//        inlineKeyboardMarkup.setKeyboard(rowList);
+//        message.setText("выберите задачу, которую нужно удалить: ");
+//        message.setReplyMarkup(inlineKeyboardMarkup);
 //
-//        List<InlineKeyboardButton> keyboardButtonsRow2 = new ArrayList<>();
-////        keyboardButtonsRow1.add(new InlineKeyboardButton().setText("Fi4a").setCallbackData("CallFi4a"));
-//        keyboardButtonsRow2.add(inlineKeyboardButton2);
-//        rowList.add(keyboardButtonsRow2);
-        inlineKeyboardMarkup.setKeyboard(rowList);
-        message.setText("выберите задачу, которую нужно удалить: ");
-        message.setReplyMarkup(inlineKeyboardMarkup);
-
-        return message;
-    }
-    private String callbackData (BotTask task/*,SendMessage message, AbsSender absSender*/){
-        task.deleteTask(task);
-        return "Задание " + task.getBotMessage().getMessge() + " удалено!";
-//        sendMess(absSender, message);
-    }
+//        return message;
+//    }
+//    private String callbackData (BotTask task/*,SendMessage message, AbsSender absSender*/){
+//        task.deleteTask(task);
+//        return "Задание " + task.getBotMessage().getMessge() + " удалено!";
+////        sendMess(absSender, message);
+//    }
 }
